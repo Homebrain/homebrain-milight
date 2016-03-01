a7c66fcb1230cc5821cd0a10424e983a1c4c84d9
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-03-01 18:57:49 +0100

    More features, started working on Homebrain integration

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from main import MiLight
+
+agentclass = MiLight
+autostart = True
--- main.py
+++ main.py
@@ -1,44 +1,14 @@
-import socket
-from time import sleep
+import milight as mi
 
-_ZONE_ARRAY = [0x42, 0x45, 0x47, 0x49, 0x4B]
+from homebrain import Agent
 
-def _get_zone(zone):
-    return _ZONE_ARRAY[zone]
+class MiLight(Agent):
+    def __init__(self):
+        super(MiLight, self).__init__()
 
-def _msg(b1, b2=0x00, b3=0x55):
-    return bytes([b1, b2, b3])
+    def handle_event(self, event):
+        print(event)
 
-def on(zone):
-    msg = _msg(_get_zone(zone))
-    return msg
 
-def off(zone):
-    if zone == 0:
-        return _msg(0x41)
-    else:
-        return _msg(_get_zone(zone)+1)
 
-def _print_cmd(cmd):
-    print(list(map(hex, cmd)))
 
-def send_cmd(cmd):
-    _print_cmd(cmd)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    sock.sendto(cmd, ('255.255.255.255', 8899))
-
-def blink(loop=False):
-    while True:
-        ZONE = 0
-        SLEEP_TIME = 1
-        send_cmd(on(ZONE))
-        sleep(SLEEP_TIME)
-        send_cmd(off(ZONE))
-        if not loop:
-            break
-        sleep(SLEEP_TIME)
-
-if __name__ == "__main__":
-    blink(loop=False)
