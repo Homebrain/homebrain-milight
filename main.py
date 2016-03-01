d348227b9bbea2dcfdb2b49cfb4bd23c17673fd8
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-03-01 19:39:54 +0100

    Set target, so no more crashes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from homebrain import Agent
 class MiLight(Agent):
     def __init__(self):
         super(MiLight, self).__init__()
+        self.target = self.identifier
 
     def handle_event(self, event):
         print(event)
