0655822ff1fa350a40df7d9ef6e9e665d8b218b0
Johan Bjäreholt <johanthepro@gmail.com>
2016-03-20 22:06:36 +0100

    Fixed fade and blink actions not working

--- miactions.py
+++ miactions.py
@@ -42,30 +42,27 @@ def hue(zone, hue):
 def blink(zone=0, loop=False, sleeptime=1):
     while True:
         ZONE = 0
-        send_cmd(on(zone))
+        on(zone)
         sleep(sleeptime)
-        send_cmd(off(zone))
+        off(zone)
         if not loop:
             break
         sleep(sleeptime)
 
 def strobe(zone, time):
-    blink(zone, time)
+    blink(zone, True, time)
 
 def fade_brightness(zone, time, fadein=True):
     """
     Fades in from lowest to highest during a total of the passed argument time.
     If argument fadein is set to False it will instead fade out from the highest to lowest.
     """
-    # Select zone
-    msg = milight.on(zone)
-    milight.send_cmd(msg)
     # Loop brightness levels
-    step = time/BRIGHTNESS_LEVELS
-    for i in range(BRIGHTNESS_LEVELS):
+    step = time/milight.BRIGHTNESS_LEVELS
+    for i in range(milight.BRIGHTNESS_LEVELS):
         if not fadein:
-            i = (BRIGHTNESS_LEVELS-1) - i
-        send_cmd(brightness(i))
+            i = (milight.BRIGHTNESS_LEVELS-1) - i
+        brightness(zone, i)
         sleep(step)
 
 
